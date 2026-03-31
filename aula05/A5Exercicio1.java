e5d08446f8fd74883cd23856f2871969d4a917ad
Marcelo <marc.m.amorim@gmail.com>
2026-03-31 19:00:05 -0300

    v1

--- aula05/A5Exercicio1.java
+++ aula05/A5Exercicio1.java
@@ -3,19 +3,17 @@ import java.util.Scanner;
 public class A5Exercicio1 {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
+        String texto;
 
         System.out.println("Digite um texto:");
-        String texto = leitor.nextLine();
-        System.out.println("O texto digitado é: " + texto);
+        texto = leitor.nextLine();
+        System.out.println("Você digitou: "+texto);
         int qtdeLetras = texto.length();
-        System.out.println("Qtde de letras: " + qtdeLetras);
-        System.out.println("Primeira Letra: " + texto.charAt(0));
-        System.out.println("Última Letra: " + texto.charAt(qtdeLetras-1));
-        System.out.println("Texto em Maiúsculo: " + texto.toUpperCase());
-        System.out.println("Texto em Minúsculo: " + texto.toLowerCase());
+        System.out.println("O texto tem: "+qtdeLetras+" letras");
+        System.out.println("A primeira letra é: "+texto.charAt(0));
+        System.out.println("A última letra é: "+texto.charAt(qtdeLetras-1));
+        System.out.println("Texto Maiusculo: "+texto.toUpperCase());
+        System.out.println("Texto Minusculo: "+texto.toLowerCase());
 
-        if(texto.equals("Oi gente")) {
-
-        }
     }
 }
---
+++ aula06/readme.md
@@ -0,0 +1,5 @@
+<img src="/assets/teste.svg" width="100%">
+
+# Aula 6 - 31/03/2026
+
+[Exercício do Jogo do Palitinho](./ExercicioJogoPalitinhos.md) 
