99ff5f306128f96300e98b05f015281f4f3dceb7
Marcelo <marc.m.amorim@gmail.com>
2026-04-08 08:49:31 -0300

    v1

---
+++ aula07/ExercicioA701.java
@@ -0,0 +1,11 @@
+public class ExercicioA701 {
+    
+    public static void main(String[] args) {
+
+        String texto = "Alguma coisa";
+
+        for(int i=0; i< texto.length(); i++) {
+            System.out.println(texto.charAt(i));
+        }
+    }
+}
