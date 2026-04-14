7678b386e22b63a1d5b78252d23c8c13ffbc83c1
Marcelo <marc.m.amorim@gmail.com>
2026-04-14 20:57:48 -0300

    v1

--- aula08/SuperCaixa.java
+++ aula08/SuperCaixa.java
@@ -1,12 +1,18 @@
+import java.util.Scanner;
+
 public class SuperCaixa {
 
     static String produtos[] = { "Feijão", "Café", "Arroz 5Kg", "Snickers", "Farinha 1kg", "Coca-cola" };
     static double precos[] = { 7.5, 30.75, 20.0, 5.5, 8.0, 15.0 };
 
     public static void main(String[] args) {
-
+        
         imprimeNomeAppVersao();
         imprimeListaProdutos();
+
+        int codigo = lerCodigo();
+        int qtde = lerQtde();
+        imprimeItemLinha(codigo,qtde);
 
     }
 
@@ -19,9 +25,28 @@ public class SuperCaixa {
     public static void imprimeListaProdutos() {
         System.out.println("MENU - Lista de produtos");
         System.out.println("========================");
-        for(int i=0; i < produtos.length; i++) {
-            System.out.println("("+i+") - "+produtos[i]+" R$ "+precos[i]);
+        for (int i = 0; i < produtos.length; i++) {
+            System.out.println("(" + i + ") - " + produtos[i] + " R$ " + precos[i]);
         }
     }
 
+    public static void imprimeItemLinha(int codigo, int qtde) {
+        String linha = "item: (" + codigo + ") - " + produtos[codigo];
+        linha = linha + " - " + qtde + " unidades";
+        System.out.println(linha);
+    }
+
+    public static int lerCodigo() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite código do produto: ");
+        int codigo = sc.nextInt();
+        return codigo;
+    }
+
+    public static int lerQtde() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite QTDE: ");
+        int qtde = sc.nextInt();
+        return qtde;
+    }
 }
