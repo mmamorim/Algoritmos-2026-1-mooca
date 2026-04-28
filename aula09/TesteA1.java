c122bfc9d05edf6629f2c2f65dd5a6a5ac35e0e4
Marcelo <marc.m.amorim@gmail.com>
2026-04-28 19:55:40 -0300

    v1

---
+++ aula09/TesteA1.java
@@ -0,0 +1,11 @@
+public class TesteA1 {
+
+    public static void main(String[] args) {
+        int idade = 20;
+        String nome = "Ana";
+
+        Screen.clear();
+        System.out.println("Oi "+nome+" vc fará "+(idade+1));
+    }
+    
+}
